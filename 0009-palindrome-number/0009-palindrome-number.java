1a9e0075ac3c548db10cf6b878959db563cee395
Ayush Mehta <ayushmehta800006@gmail.com>
2026-06-30 22:41:38 +0530

    Time: 4 ms (100%), Space: 45.8 MB (86.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,14 @@
 class Solution {
     public static boolean isPalindrome(int x) {
         int temp = x, rev = 0;
-        if(x < 0) {
-            return false;
-        } else {
-            while(x > 0) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
-        }
-        if(rev == temp) {
-            return true;
-        } else {
-            return false;
-        }
-        }      
+            while (x > 0) {
+                rev = rev * 10 + x % 10;
+                x /= 10;
+            }
+            if (rev == temp) {
+                return true;
+            } else {
+                return false;
+            }
     }
 }
